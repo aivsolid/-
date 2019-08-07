39b98f3243eb90b01657aef18915e5daa52cb57d
aivsolid <aivsolid@gmail.com>
2019-08-07 13:55:49 +0700

    Add files via upload

---
+++ xsd2code.ps1
@@ -0,0 +1,8 @@
+﻿Set-Location D:/projects/xsd_egais
+$sourceFiles = (Get-ChildItem D:/projects/xsd_egais/ -filter "*.xsd")
+foreach($file in $sourceFiles) {
+    $fileName = $file.BaseName+'.cs'
+    $namespace = 'Egais.Entities.'+$file.BaseName
+    D:/projects/xsd_egais/Xsd2Code.exe $file $namespace $fileName /pl Net35 /xa /is /dc+ /db /eit    
+    #echo $fileName
+}
