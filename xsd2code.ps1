ec4f5274c1c96750f5aff44039df2336697d0fb1
aivsolid <aivsolid@gmail.com>
2019-08-07 13:58:31 +0700

    Update xsd2code.ps1

--- xsd2code.ps1
+++ xsd2code.ps1
@@ -4,5 +4,4 @@ foreach($file in $sourceFiles) {
     $fileName = $file.BaseName+'.cs'
     $namespace = 'Egais.Entities.'+$file.BaseName
     D:/projects/xsd_egais/Xsd2Code.exe $file $namespace $fileName /pl Net35 /xa /is /dc+ /db /eit    
-    #echo $fileName
 }
